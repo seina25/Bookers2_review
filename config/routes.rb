f59a453e569f8dc486f85b5253cc1f9989079e21
seina25 <replace421@gmail.com>
2021-06-29 20:20:05 +0900

    [update]book#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
   resources :books do
     resources :book_comments, only: [:create, :destroy]
-
     resource :favorites, only: [:create, :destroy]
   end
 
