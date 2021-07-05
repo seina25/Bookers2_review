2c8085d618ec27d6617e1b7f126bdd2408621c16
seina25 <replace421@gmail.com>
2021-07-05 12:11:50 +0900

    [update]book#show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  
   def after_sign_in_path_for(resource)
     current_user
   end
@@ -12,6 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email] )
   end
+  
 end
