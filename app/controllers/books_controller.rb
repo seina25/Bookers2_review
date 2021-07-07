81fce0b98368d9090a55ef94a15687bab84992ce
seina25 <replace421@gmail.com>
2021-07-07 10:22:50 +0900

    [update]book#show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book), notice: "You have created book successfully!"
     else
       @books = Book.all
       render 'index'
@@ -34,7 +34,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      redirect_to book_path(@book), notice: "You have updated book successfully!!"
     else
       render "edit"
     end
